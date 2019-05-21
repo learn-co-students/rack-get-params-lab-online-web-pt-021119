ab0ba4adddfa7b23c006e271cb59c1f618d478a3
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-21 22:20:53 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -11,38 +11,57 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/list/)
-      @@cart.each do |item|
-        resp.write "#{list}"
+
+    elsif req.path.match(/cart/)
+      @@cart.each do |list|
+        resp.write "#{list}\n"
       end
-    elsif @@cart.empty?
-      resp.write "Your cart is empty"
+
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      end
+
     elsif req.path.match(/search/)
 
-      search_term = req.params{"q"}
+      search_term = req.params["q"]
 
       if @@cart.include?(search_term)
         resp.write "#{search_term}"
       elsif
         resp.write "Path not found"
       end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    else
-      resp.write "Path Not Found"
-    end
 
-    resp.finish
+
+    elsif req.path.match(/add/)
+
+      item = req.param["q"]
+
+      if @@cart.include?(item)
+        @@cart << item
+        resp.write "added #{item}"
+      else
+        resp.write "We don't have that item"
+      end
+    else
+      resp.write "Path not found"
+    end
   end
 
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
 
-  @@cart = []
-end
+
+
+
+    # elsif req.path.match(/search/)
+    #   search_term = req.params["q"]
+    #   resp.write handle_search(search_term)
+    # else
+    #   resp.write "Path Not Found"
+    # end
+
+
+  # def handle_search(search_term)
+  #   if @@items.include?(search_term)
+  #     return "#{search_term} is one of our items"
+  #   else
+  #     return "Couldn't find #{search_term}"
+ end
